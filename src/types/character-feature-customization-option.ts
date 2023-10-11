8c1bac88a8d8d0574e14ce81245196124f82ac56
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-10-10 21:43:43 -0500

    Rename CharacterEvents

--- src/types/character-feature-customization-option.ts
+++ src/types/character-feature-customization-option.ts
@@ -1,10 +1,10 @@
-export enum CharacterEvents {
-    SET_CLASS,
-    SET_RACE,
-    SET_BACKGROUND,
-    SET_ABILITY_SCORES,
-    LEVEL_UP,
-    MULTICLASS,
+export enum CharacterPlannerStep {
+    SET_CLASS = 'SET_CLASS',
+    SET_RACE = 'SET_RACE',
+    SET_BACKGROUND = 'SET_BACKGROUND',
+    SET_ABILITY_SCORES = 'SET_ABILITY_SCORES',
+    LEVEL_UP = 'LEVEL_UP',
+    MULTICLASS = 'MULTICLASS',
     CHOOSE_SUBRACE = 'CHOOSE_SUBRACE',
     CHOOSE_SUBCLASS = 'CHOOSE_SUBCLASS',
 }
@@ -14,5 +14,5 @@ export interface ICharacterFeatureCustomizationOption {
     description?: string;
     choices?: ICharacterFeatureCustomizationOption[][];
     image?: string;
-    choiceType?: CharacterEvents;
+    choiceType?: CharacterPlannerStep;
 }
