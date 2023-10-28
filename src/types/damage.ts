ed0fbe552de60818ab2083c0a8839da4eb160217
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-10-28 15:26:48 -0500

    add damage text colors

--- src/types/damage.ts
+++ src/types/damage.ts
@@ -14,3 +14,20 @@ export enum DamageType {
     Force,
     Psychic,
 }
+
+export const damageTypeColor: Record<DamageType, string> = {
+    [DamageType.NONE]: '#fff',
+    [DamageType.Slashing]: '#8c8c8c',
+    [DamageType.Piercing]: '#8c8c8c',
+    [DamageType.Bludgeoning]: '#8c8c8c',
+    [DamageType.Fire]: '#ee5500',
+    [DamageType.Cold]: '#3399cc',
+    [DamageType.Lightning]: '#3366cc',
+    [DamageType.Thunder]: '#8844bb',
+    [DamageType.Acid]: '#80b000',
+    [DamageType.Poison]: '#44bb00',
+    [DamageType.Radiant]: '#ccaa00',
+    [DamageType.Necrotic]: '#40b050',
+    [DamageType.Force]: '#cc3333',
+    [DamageType.Psychic]: '#cc77aa',
+};
