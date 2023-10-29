e874da70d74194eaaebb4b2df3e207d7683f2146
Jorgen Swiderski <jorgen.swiderski@gmail.com>
2023-10-29 14:17:24 -0500

    Only show "save" menu item if the user is authorized to edit

--- src/types/builds.ts
+++ src/types/builds.ts
@@ -4,9 +4,10 @@ export interface Build {
     encoded: string;
     version: string;
     id: BuildId;
+    mayEdit?: boolean;
 }
 
-export interface BuildWithInfo extends Build {
+export interface BuildWithInfo extends Omit<Build, 'mayEdit'> {
     createdUtc: number;
     hits: number;
     lastAccessedUtc: number;
